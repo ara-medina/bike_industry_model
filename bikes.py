c61751610c53f9765ccdaf486738a4db4f7960ae
Sandra Medina Corey <s.medinacorey@gmail.com>
2016-11-13 11:08:28 -0500

    initial commit

---
+++ bikes.py
@@ -0,0 +1,103 @@
+import random
+
+class Bicycle(object):
+	def __init__(self, name, weight, cost):
+		self.name = name
+		self.weight = weight
+		self.cost = cost
+
+
+class Shop(object):
+
+	def __init__(self, inventory):
+		self.inventory = inventory
+		self.prices = {}
+		self.can_buy = {}
+		self.in_stock = {}
+
+	def stock(self, stock_list):
+		bikes = []
+
+		for model in self.inventory:
+			bikes.append(model.name)
+
+		self.in_stock = {}
+
+		for i in range(len(stock_list)):
+			self.in_stock[bikes[i]] = stock_list[i]
+
+
+	def price(self):
+		for model in self.inventory:
+			bike_cost = model.cost * 1.2
+			self.prices[model.name] = bike_cost
+
+		print(self.prices)
+
+	def afford(self, customer):
+		print(customer.name)
+
+		for model, price in self.prices.items():
+			if customer.budget > price:
+				self.can_buy[model] = price
+
+		print(self.can_buy)
+
+	def postPurchase(self, customer):
+		for model in self.inventory:
+			if model.name == customer.purchased[0]:
+				self.in_stock[model.name] = self.in_stock[model.name] - 1
+				print(self.in_stock[model.name])
+
+
+class Customer(object):
+	def __init__(self, name, budget):
+		self.name = name
+		self.budget = budget
+		self.purchased = None
+
+	def purchase(self, shop):
+		self.purchased = random.choice(list(shop.can_buy.items()))
+		print(self.purchased)
+
+		remaining_funds = self.budget - self.purchased[1]
+		print(remaining_funds)
+
+		
+
+
+# Bikes 
+Model_100 = Bicycle("Model 100", 11, 100)
+Model_200 = Bicycle("Model 200", 12, 200)
+Model_300 = Bicycle("Model 300", 13, 300)
+Model_400 = Bicycle("Model 400", 14, 400)
+Model_500 = Bicycle("Model 500", 15, 500)
+Model_600 = Bicycle("Model 600", 16, 600)
+Model_700 = Bicycle("Model 700", 17, 700)
+
+# Customers 
+bob = Customer("Bob", 200)
+stuart = Customer("Stuart", 500)
+fred = Customer("Fred", 1000)
+
+# Shops
+allegro = Shop([Model_100, Model_200, Model_300, Model_400, Model_500, Model_600])
+
+# Examples
+allegro.price()
+allegro.afford(bob)
+allegro.stock([101, 102, 103, 104, 105, 106])
+bob.purchase(allegro)
+allegro.postPurchase(bob)
+
+
+
+
+
+
+
+
+
+
+
+
